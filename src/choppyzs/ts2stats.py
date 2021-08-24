1eae2ff108aaa6077c9c6a2741e7cd0c93eae865
Mark Coletti <colettima@ornl.gov>
2021-08-23 22:43:24 -0400

    Cut out dead code.

--- src/choppyzs/ts2stats.py
+++ src/choppyzs/ts2stats.py
@@ -27,11 +27,10 @@ except ImportError:
 logger = create_logger()
 
 
-def _init_pool(b_df, b_db, scpdsi_data, affine_data):
+def _init_pool(b_df, scpdsi_data, affine_data):
     """ Used to set up data for workers """
-    global boundaries_df, boundaries_db, scpdsi, affine
+    global boundaries_df, scpdsi, affine
     boundaries_df = b_df
-    boundaries_db = b_db
     scpdsi = scpdsi_data
     affine = affine_data
 
@@ -54,7 +53,7 @@ def _agg_ts_data(nc_time):
 
     # Note that we use the fiona database instead of the shape file,
     # which save a little bit of time.
-    stats_data = zonal_stats(boundaries_db,
+    stats_data = zonal_stats(boundaries_df,
                              nc_arr_values, affine=affine,
                              stats='min,max,mean,median,majority,sum,std,count,range'.split(
                                  ','),
@@ -79,40 +78,20 @@ def ts_to_stats(shape_file_archive, ts_file):
     """
 
     def read_shapefile(shape_file_archive):
-        """ Create a geopandas dataframe and a fiona db from shapefile
+        """ Create a geopandas dataframe from shapefile
 
         :param shape_file_archive: containing boundaries
-        :return: geopandas dataframe and fiona db of boundaries
+        :return: geopandas dataframe
         """
-        # First unroll the shape file into a temporary directory,
-        # and then extract the shapefile from it.
-        with TemporaryDirectory() as temp_dir:
-            patoolib.extract_archive(shape_file_archive, outdir=temp_dir)
+        logger.info(f'Reading {shape_file_archive}')
 
-            # TODO Wrap this in exception handling in case there are
-            # no found shape files.
-            shape_file = list(Path(temp_dir).glob('*.shp'))[0]
+        # We want this geopandas dataframe since we're going to be
+        # using it over and over again to build the returned
+        # dataframe. Note that geopandas is smart enough to grab the
+        # file from inside an archive.
+        boundaries_df = gpd.read_file(shape_file_archive)
 
-            logger.info(f'Reading {shape_file}')
-
-            # We want this geopandas dataframe since we're going to be
-            # using it over and over again to build the returned
-            # dataframe. Note that geopandas is smart enough to grab the
-            # file from inside an archive.
-            boundaries_df = gpd.read_file(shape_file_archive)
-            # We don't need the geometry here; we just want the salient
-            # dataframe columns that will later be written.
-            # boundaries_df.drop(columns='geometry',
-            #                    inplace=True,
-            #                    errors='ignore')
-
-            # We want to read the shape file separately into a local
-            # spatial database to quickly do geoprocessing.  And we
-            # do this once right here so we're not constantly re-reading
-            # this information.
-            boundaries_db = fiona.open(shape_file)
-
-            return boundaries_df, boundaries_db
+        return boundaries_df
 
     def read_ts(ts_file):
         """ read the time series data file
@@ -128,73 +107,39 @@ def ts_to_stats(shape_file_archive, ts_file):
 
         return drought_ts, affine
 
-    def chop(boundaries_df, boundaries_db, drought_ts, affine):
+    def chop(boundaries_df, drought_ts, affine):
         """ perform the data aggregation
         :return: dataframe of aggregated data
         """
         scpdsi = drought_ts['scpdsi']
         times = drought_ts['time'].values
-        dfs = []
-
-        # json = boundaries_df.to_json()
 
         with ProcessPoolExecutor(initializer=_init_pool,
                                  initargs=(boundaries_df,
-                                           boundaries_df,
                                            scpdsi,
                                            affine)) as pool:
             df_futures = pool.map(_agg_ts_data,
                                   times)
 
-            # for nc_time in tqdm(times):
-            #     # logger.info(f'Parsing time {nc_time}')
-            #     nc_arr = scpdsi.sel(time=nc_time)
-            #     nc_arr_values = nc_arr.values
-            #
-            #     # Note that we use the fiona database instead of the shape file,
-            #     # which save a little bit of time.
-            #     stats_data = zonal_stats(boundaries_db,
-            #                              nc_arr_values, affine=affine,
-            #                              stats='min,max,mean,median,majority,sum,std,count,range'.split(','),
-            #                              nodata=-9999, # to shut up stupid warning
-            #                              geojson_out=False,
-            #                              all_touched=True)
-            #     sd = pd.DataFrame.from_dict(stats_data)
-            #
-            #     # We also save a little time by just reusing the static dataframe
-            #     # instead of running through a constructor over and over again.
-            #     # df = pd.DataFrame(self.shape_df)
-            #     dat = pd.concat([boundaries_df, sd], axis=1)
-            #
-            #     dat['time'] = nc_time
-            #
-            #     dfs.append(dat)
             dfs = list(df_futures)
-
-            # for future in as_completed(df_futures):
-            #     try:
-            #         nc_time, from_dict = future.result()
-            #         logger.info(f'Processed for time {nc_time}')
-            #         dfs.append(pd.DataFrame.from_records(from_dict))
-            #     except Exception as e:
-            #         print(f'Exception for future.result(): {e}')
 
         logger.info(f'Have {len(dfs)} dataframes.')
 
         return pd.concat(dfs, ignore_index=True)
+        # end chop()
 
     check_if_file_exists(shape_file_archive)
     check_if_file_exists(ts_file)
 
     # Extra a geopandas dataframe and a fiona database from the
     # given shape file.
-    boundaries_df, boundaries_db = read_shapefile(shape_file_archive)
+    boundaries_df = read_shapefile(shape_file_archive)
 
     # Now grab the time series data
     drought_ts, affine = read_ts(ts_file)
 
     # Now do the chopping
-    df = chop(boundaries_df, boundaries_db, drought_ts, affine)
+    df = chop(boundaries_df, drought_ts, affine)
 
     df.drop(columns='geometry',
             inplace=True,
