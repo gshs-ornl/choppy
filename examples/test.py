49c28767b89b050f5bc1e84b443545dd6057e576
Mark Coletti <colettima@ornl.gov>
2021-08-23 22:53:38 -0400

    Fixed pathlib usage.

--- examples/test.py
+++ examples/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from pathlib import Path
 from choppyzs.logz import create_logger
 logger = create_logger()
 
@@ -11,11 +11,13 @@ from choppyzs.ts2stats import ts_to_stats
 if __name__ == "__main__":
     start_time = perf_counter()
 
+    shape_file = Path('lichtenstein.zip')
+
     # nts = NTS('Shapes.zip', 'drought.nc')
-    df = ts_to_stats('lichtenstein.zip', 'drought.nc')
+    df = ts_to_stats(shape_file, 'drought.nc')
 
     if df is not None and not df.empty:
-        df.to_csv('zonal_stats.csv', index=False)
+        df.to_csv(shape_file.with_suffix('.csv'), index=False)
     else:
         logger.warning('No data to write.')
 
