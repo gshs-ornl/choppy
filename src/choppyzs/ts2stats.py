beacda59f243f851ea56a1db90202a4d5f6cbfcc
Mark Coletti <colettima@ornl.gov>
2021-08-23 21:49:34 -0400

    Works, but still takes 12 minutes.

--- src/choppyzs/ts2stats.py
+++ src/choppyzs/ts2stats.py
@@ -5,7 +5,8 @@
 """
 from tempfile import TemporaryDirectory
 from pathlib import Path
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed, \
+    ThreadPoolExecutor
 
 import patoolib
 import xarray
@@ -38,16 +39,16 @@ def init_pool(b_df, b_db, scpdsi_data, affine_data):
 def agg_ts_data(nc_time):
     """ This creates a single dataframe for the given nc_time
 
-    :return: single dataframe of nc_time data
+    :returns: time and single dataframe of nc_time data
     """
-    try:
-        from choppyzs.logz import create_logger
-    except ImportError:
-        from .logz import create_logger
-
-    logger = create_logger()
-    logger.info(f'Parsing time {nc_time}')
-
+    # try:
+    #     from choppyzs.logz import create_logger
+    # except ImportError:
+    #     from .logz import create_logger
+    #
+    # logger = create_logger()
+    # logger.info(f'Parsing time {nc_time}')
+    #
     nc_arr = scpdsi.sel(time=nc_time)
     nc_arr_values = nc_arr.values
 
@@ -96,13 +97,14 @@ def ts_to_stats(shape_file_archive, ts_file):
 
             # We want this geopandas dataframe since we're going to be
             # using it over and over again to build the returned
-            # dataframe.
-            boundaries_df = gpd.read_file(shape_file)
+            # dataframe. Note that geopandas is smart enough to grab the
+            # file from inside an archive.
+            boundaries_df = gpd.read_file(shape_file_archive)
             # We don't need the geometry here; we just want the salient
             # dataframe columns that will later be written.
-            boundaries_df.drop(columns='geometry',
-                               inplace=True,
-                               errors='ignore')
+            # boundaries_df.drop(columns='geometry',
+            #                    inplace=True,
+            #                    errors='ignore')
 
             # We want to read the shape file separately into a local
             # spatial database to quickly do geoprocessing.  And we
@@ -119,7 +121,7 @@ def ts_to_stats(shape_file_archive, ts_file):
         logger.info(f'Reading {ts_file}')
 
         # Get the time series of drought data
-        drought_ts = xarray.open_dataset(ts_file)
+        drought_ts = xarray.load_dataset(ts_file)
 
         # Bendy affine transform data
         affine = rasterio.open(ts_file).transform
@@ -134,36 +136,48 @@ def ts_to_stats(shape_file_archive, ts_file):
         times = drought_ts['time'].values
         dfs = []
 
-        with ProcessPoolExecutor(initializer=init_pool,
-                                 initargs=(boundaries_df, boundaries_db, scpdsi,
-                                           affine)) as pool:
-            dfs = pool.map(agg_ts_data, times)
+        # json = boundaries_df.to_json()
 
-        # for nc_time in tqdm(times):
-        #     # logger.info(f'Parsing time {nc_time}')
-        #     nc_arr = scpdsi.sel(time=nc_time)
-        #     nc_arr_values = nc_arr.values
-        #
-        #     # Note that we use the fiona database instead of the shape file,
-        #     # which save a little bit of time.
-        #     stats_data = zonal_stats(boundaries_db,
-        #                              nc_arr_values, affine=affine,
-        #                              stats='min,max,mean,median,majority,sum,std,count,range'.split(','),
-        #                              nodata=-9999, # to shut up stupid warning
-        #                              geojson_out=False,
-        #                              all_touched=True)
-        #     sd = pd.DataFrame.from_dict(stats_data)
-        #
-        #     # We also save a little time by just reusing the static dataframe
-        #     # instead of running through a constructor over and over again.
-        #     # df = pd.DataFrame(self.shape_df)
-        #     dat = pd.concat([boundaries_df, sd], axis=1)
-        #
-        #     dat['time'] = nc_time
-        #
-        #     dfs.append(dat)
+        with ThreadPoolExecutor(initializer=init_pool,
+                                initargs=(boundaries_df,
+                                          boundaries_db,
+                                          scpdsi,
+                                          affine)) as pool:
+            df_futures = pool.map(agg_ts_data,
+                                  times)  # :5 just to temp get first 5
 
-        dfs = list(dfs)
+            # for nc_time in tqdm(times):
+            #     # logger.info(f'Parsing time {nc_time}')
+            #     nc_arr = scpdsi.sel(time=nc_time)
+            #     nc_arr_values = nc_arr.values
+            #
+            #     # Note that we use the fiona database instead of the shape file,
+            #     # which save a little bit of time.
+            #     stats_data = zonal_stats(boundaries_db,
+            #                              nc_arr_values, affine=affine,
+            #                              stats='min,max,mean,median,majority,sum,std,count,range'.split(','),
+            #                              nodata=-9999, # to shut up stupid warning
+            #                              geojson_out=False,
+            #                              all_touched=True)
+            #     sd = pd.DataFrame.from_dict(stats_data)
+            #
+            #     # We also save a little time by just reusing the static dataframe
+            #     # instead of running through a constructor over and over again.
+            #     # df = pd.DataFrame(self.shape_df)
+            #     dat = pd.concat([boundaries_df, sd], axis=1)
+            #
+            #     dat['time'] = nc_time
+            #
+            #     dfs.append(dat)
+            dfs = list(df_futures)
+
+            # for future in as_completed(df_futures):
+            #     try:
+            #         nc_time, df = future.result()
+            #         logger.info(f'Processed for time {nc_time}')
+            #         dfs.append(df)
+            #     except Exception as e:
+            #         print(f'Exception for future.result(): {e}')
 
         logger.info(f'Have {len(dfs)} dataframes.')
 
@@ -182,6 +196,10 @@ def ts_to_stats(shape_file_archive, ts_file):
     # Now do the chopping
     df = chop(boundaries_df, boundaries_db, drought_ts, affine)
 
+    df.drop(columns='geometry',
+            inplace=True,
+            errors='ignore')
+
     return df
 
 
@@ -195,7 +213,7 @@ if __name__ == '__main__':
         df = ts_to_stats('examples/lichtenstein.zip',
                          'examples/drought.nc')
 
-    if df is not none and not df.empty:
+    if df is not None and not df.empty:
         df.to_csv('new_zonal_stats.csv')
     else:
         logger.warning('No data to write.')
